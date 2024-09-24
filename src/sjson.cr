35369502adf893481d66ae4efd1989dca0caeb37
Taufik <taufik@academiaworks.id>
2024-09-24 11:36:46 +0700

    menambahkan json

--- src/sjson.cr
+++ src/sjson.cr
@@ -1,4 +1,5 @@
-# TODO: Write documentation for `Sjson`
+require "json"
+
 module SJSON
   VERSION = "0.1.0"
 
@@ -8,19 +9,19 @@ module SJSON
   end
 
   def self.stringToHash(str : String)
-      hash = {} of String => String
-      if !str.empty? && !str.nil?
-          str[1..-2].split(", ").each do |pair|
-              key, value = pair.split(" => ")
-              key = key[1..-2] if key.starts_with?("\"") && key.ends_with?("\"")
-              if value.starts_with?("\"") && value.ends_with?("\"")
-                  value = value[1..-2]
-                  hash[key] = value
-              elsif value =~ /^\d+$/
-                  hash[key] = value.to_s
-              else
-                  hash[key] = value
-              end
+    hash = {} of String => String
+    if !str.empty? && !str.nil?
+        str[1..-2].split(", ").each do |pair|
+            key, value = pair.split(" => ")
+            key = key[1..-2] if key.starts_with?("\"") && key.ends_with?("\"")
+            if value.starts_with?("\"") && value.ends_with?("\"")
+                value = value[1..-2]
+                hash[key] = value
+            elsif value =~ /^\d+$/
+                hash[key] = value.to_s
+            else
+                hash[key] = value
+            end
           end
       end
       hash
